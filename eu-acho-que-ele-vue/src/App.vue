f392cceaf2e7d4740f048ccdea490119a379797e
alohamat <matalohazk@gmail.com>
2026-08-06 20:33:03 -0300

    nova atividade

---
+++ eu-acho-que-ele-vue/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>Calculadora</h1>
+
+    <input
+      type="number"
+      v-model.number="numero1"
+      placeholder="Primeiro número"
+    />
+
+    <input
+      type="number"
+      v-model.number="numero2"
+      placeholder="Segundo número"
+    />
+
+    <br><br>
+
+    <button @click="calcular('+')">+</button>
+    <button @click="calcular('-')">-</button>
+    <button @click="calcular('*')">*</button>
+    <button @click="calcular('/')">/</button>
+
+    <hr>
+
+    <h2>Histórico</h2>
+
+    <ul>
+      <Conta
+        v-for="(conta, index) in historico"
+        :key="index"
+        :numero1="conta.numero1"
+        :numero2="conta.numero2"
+        :operador="conta.operador"
+      />
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Conta from './components/Conta.vue'
+
+const numero1 = ref(0)
+const numero2 = ref(0)
+
+const historico = ref([])
+
+function calcular(operador) {
+  historico.value.push({
+    numero1: numero1.value,
+    numero2: numero2.value,
+    operador
+  })
+}
+</script>
---
+++ eu-acho-que-ele-vue/src/components/Conta.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    {{ numero1 }} {{ operador }} {{ numero2 }} = {{ resultado }}
+  </li>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  numero1: Number,
+  numero2: Number,
+  operador: String
+})
+
+const resultado = computed(() => {
+  switch (props.operador) {
+    case '+':
+      return props.numero1 + props.numero2
+
+    case '-':
+      return props.numero1 - props.numero2
+
+    case '*':
+      return props.numero1 * props.numero2
+
+    case '/':
+      return props.numero2 !== 0
+        ? props.numero1 / props.numero2
+        : 'Erro'
+
+    default:
+      return 'Inválido'
+  }
+})
+</script>
